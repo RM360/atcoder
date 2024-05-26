f0e1cf9132867b07bba88f5e048094cf3a1a2362
ryo maruyama <ryomaruyama@ryo-maruyamanoMacBook-Pro.local>
2024-05-26 14:49:25 +0900

    ABC085C_Otoshidama

---
+++ beginners-selection/ABC085C_Otoshidama/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 回答を見た
+// https://atcoder.jp/contests/abs/tasks/abc085_c/editorial
+func main() {
+
+	var N, Y int
+	fmt.Scanf("%d %d", &N, &Y)
+
+	num10000, num5000, num1000 := -1, -1, -1
+
+	for a := 0; a <= N; a++ {
+		for b := 0; b+a <= N; b++ {
+
+			c := N - a - b
+			total := 10000*a + 5000*b + 1000*c
+
+			if total == Y {
+				num10000 = a
+				num5000 = b
+				num1000 = c
+			}
+			// 1 秒間で処理できる for 文ループの回数は、10^8回程度
+			// cの枚数はa、bが決まればcは決まるので以下のループが不要になる。
+			// for c := 0; c <= N; c++ {
+			// 	total := 10000*a + 5000*b + 1000*c
+
+			// 	if total == Y {
+			// 		num10000 = a
+			// 		num5000 = b
+			// 		num1000 = c
+			// 	}
+			// }
+		}
+	}
+
+	fmt.Println(num10000, num5000, num1000)
+}
---
+++ beginners-selection/ABC085C_Otoshidama/main_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+type testValue struct {
+	arg []string
+	ans string
+}
+
+func getTests() []testValue {
+	testValues := []testValue{
+		testValue{
+			[]string{
+				"9 45000",
+			},
+			"4 0 5"},
+		testValue{
+			[]string{
+				"20 196000",
+			},
+			"-1 -1 -1"},
+		testValue{
+			[]string{
+				"1000 1234000",
+			},
+			// "14 27 959"},
+			"26 0 974"},
+		testValue{
+			[]string{
+				"2000 20000000",
+			},
+			"2000 0 0"},
+	}
+
+	return testValues
+}
+
+func stubio(inbuf string, f func()) (string, string) {
+	inr, inw, _ := os.Pipe()
+	outr, outw, _ := os.Pipe()
+	errr, errw, _ := os.Pipe()
+
+	orgStdin := os.Stdin
+	orgStdout := os.Stdout
+	orgStderr := os.Stderr
+
+	inw.Write([]byte(inbuf))
+	inw.Close()
+
+	os.Stdin = inr
+	os.Stdout = outw
+	os.Stderr = errw
+
+	outC := make(chan string)
+	errC := make(chan string)
+	defer close(outC)
+	defer close(errC)
+	go func() {
+		var buf strings.Builder
+		io.Copy(&buf, outr)
+		outr.Close()
+		outC <- buf.String()
+	}()
+
+	go func() {
+		var buf strings.Builder
+		io.Copy(&buf, errr)
+		errr.Close()
+		errC <- buf.String()
+	}()
+
+	f()
+
+	os.Stdin = orgStdin
+	os.Stdout = orgStdout
+	os.Stderr = orgStderr
+	outw.Close()
+	errw.Close()
+
+	return <-outC, <-errC
+}
+
+func Test_main(t *testing.T) {
+	tests := getTests()
+	for i, tt := range tests {
+		si := strconv.Itoa(i)
+		t.Run("Case "+si, func(t *testing.T) {
+			ret, err := stubio(strings.Join(tt.arg, " "), main)
+			if err != "" {
+				t.Errorf("error func: %s ", err)
+			}
+			ans := fmt.Sprintf("%s\n", tt.ans)
+			if ret != ans {
+				t.Errorf("Unexpected output: '%s' Need: '%s'", ret, ans)
+			}
+		})
+	}
+}
